240a780a76cce1cf2fc445990fb3136e2447bcdd
Natalia Carvalho <carvalhonatalia@gmail.com>
2013-02-24 16:05:26 -0500

    created very basic outlines of classes that we may need

---
+++ src/view/Processable.java
@@ -0,0 +1,10 @@
+package view;
+
+/**
+ * Create interface Processable that contains data that needs to be passed
+ * (ex: contains data that the turtle needs to contain)
+ * @author Natalia Carvalho
+ */
+public interface Processable {
+
+}
