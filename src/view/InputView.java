3bdae6e43f26b27445a6f3a03a4aa77323b82efb
Eric Wu <eric.wu@duke.edu>
2013-02-24 16:09:05 -0500

    added setinputlistener method in InputView

--- src/view/InputView.java
+++ src/view/InputView.java
@@ -1,5 +1,70 @@
 package view;
 
-public class InputView {
+import java.awt.Point;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+
+/**
+ * 
+ * @author Natalia Carvalho and Eric Wu
+ *
+ */
+public class InputView extends JFrame {
+
+    private KeyListener myKeyListener;
+    private MouseListener myMouseListener;
+    private JFileChooser myFileChooser;
+    private int myLastKeyPressed;
+    
+    public static final int NO_KEY_PRESSED = -1;
+    public static final Point NO_MOUSE_PRESSED = null;
+    
+    private Point myLastMousePosition;
+
+
+
+    public InputView () {
+    }
+    
+    public void setInputListeners () {
+        
+     // initialize input state
+            myLastKeyPressed = NO_KEY_PRESSED;
+            addKeyListener(new KeyAdapter() {
+                @Override
+                public void keyPressed (KeyEvent e) {
+                    
+                }
+
+                @Override
+                public void keyReleased (KeyEvent e) {
+                    myLastKeyPressed = NO_KEY_PRESSED;
+                }
+            });
+            
+            myLastMousePosition = NO_MOUSE_PRESSED;
+            
+            addMouseMotionListener(new MouseMotionAdapter() {
+                @Override
+                public void mouseDragged (MouseEvent e) {
+                    myLastMousePosition = e.getPoint();
+                }
+            });
+            
+            addMouseListener(new MouseAdapter() {
+                @Override
+                public void mousePressed (MouseEvent e) {
+                    myLastMousePosition = e.getPoint();
+                }
+
+                @Override
+                public void mouseReleased (MouseEvent e) {
+                    myLastMousePosition = NO_MOUSE_PRESSED;
+                }
+            });
+        
+    }
 
 }
