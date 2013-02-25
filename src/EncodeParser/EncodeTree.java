c7065370f06055d57e7ab0b4b502eaab89feda27
WenshunLiu <wl88@duke.edu>
2013-02-24 22:10:33 -0500

    sherry and kevin w encodeparser

---
+++ src/EncodeParser/EncodeTree.java
@@ -0,0 +1,30 @@
+package EncodeParser;
+
+class Node{
+	String myStringValue;
+	double myDoubleValue;
+	Node myLeft;
+	Node myRight;
+	
+	public Node(double doubleValue){
+		myDoubleValue = doubleValue;
+	}
+	
+	public Node(String stringValue){
+		myStringValue = stringValue;
+	}
+}
+
+public class EncodeTree {
+	Node myHead;
+	
+	public EncodeTree(){
+		myHead = null;
+	}
+	
+	public EncodeTree(String headValue){
+		myHead.myStringValue = headValue;
+		myHead.myLeft = null;
+		myHead.myRight = null;
+	}
+}
