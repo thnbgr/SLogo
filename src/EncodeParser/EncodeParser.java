5ccc42be9958671fb30d372dca8322b26fc2562f
Kevin Oh <jo63@duke.edu>
2013-02-26 02:12:54 -0500

    working on parser

---
+++ src/EncodeParser/EncodeParser.java
@@ -0,0 +1,20 @@
+package EncodeParser;
+
+import java.util.Map;
+
+import command.CommandBundle;
+
+public abstract class EncodeParser {
+	
+	public abstract void encode(CommandBundle myPackage);
+	
+	/*
+	 * should probably not have a parser hierarchy....
+	 * TODO: 
+	 * 1. tokenize the string into operators and values
+	 * 2. syntax check
+	 * 3. build tree
+	 * 
+	 */
+	
+}
---
+++ src/EncodeParser/EncodeTree.java
@@ -0,0 +1,34 @@
+package EncodeParser;
+
+class Node{
+	String myStringValue;
+	double myDoubleValue;
+	Node myLeft;
+	Node myRight;
+
+	public Node(double doubleValue){
+		myDoubleValue = doubleValue;
+	}
+
+	public Node(String stringValue){
+		myStringValue = stringValue;
+	}
+}
+
+public class EncodeTree {
+	Node myHead;
+
+	public EncodeTree(){
+		myHead = null;
+	}
+	public Node getHead()
+	{
+		return myHead;
+	}
+
+	public EncodeTree(String headValue){
+		myHead.myStringValue = headValue;
+		myHead.myLeft = null;
+		myHead.myRight = null;
+	}
+}
