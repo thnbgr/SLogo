4578a8983da076a66695045126111a1104cdb95f
jo63 <jo63@duke.edu>
2013-02-27 07:12:48 -0500

    adding nodes

---
+++ src/parser/node/math/QuotientNode.java
@@ -0,0 +1,20 @@
+package parser.node.math;
+
+import parser.node.BinaryNode;
+import parser.node.IDoubleNode;
+import parser.node.Node;
+
+public class QuotientNode extends BinaryNode implements IDoubleNode{
+
+	public QuotientNode(Node head) {
+		super(head);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double evaluate() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
