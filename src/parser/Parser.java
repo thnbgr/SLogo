d3c27d3ad4f03d76459979db0fe1769932463b80
jo63 <jo63@duke.edu>
2013-03-04 01:10:47 -0500

    something wrong with the make?

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -12,6 +12,8 @@ import parser.node.*;
 import command.CommandBundle;
 import parser.node.turtleCommand.*;
 import java.util.ArrayList;
+import java.util.Stack;
+import java.util.Arrays;
 
 /**
  * 
@@ -21,36 +23,42 @@ public class Parser {
 	public static final String COMMAND_PROPERTIES_FILE_NAME = "commandProperties.csv";
 	private CSVTable myCSVTable;
 	private ArrayList<Node> myVariables;
+	private SyntaxCheck mySyntaxCheck;
 
 	//have it get passed in 
 	public Parser(){
 		myCSVTable = new CSVTable(COMMAND_PROPERTIES_FILE_NAME);
 		myVariables = new ArrayList<Node>();
+		mySyntaxCheck = new SyntaxCheck();
 	}
 	public EncodeTree encode(String command) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, SecurityException, InvocationTargetException{
 		command.toLowerCase();
-		StringTokenizer st = new StringTokenizer(command);
+		Queue<String> myCommandParts = new LinkedList<String>();
+		myCommandParts.addAll(Arrays.asList(command.split(" ")));
 		Queue<Node> myCurNodes = new LinkedList<Node>();
-		while(st.hasMoreTokens()){
-			String curValue = st.nextToken();
+		while(myCommandParts.size() > 0){
+			String curValue = myCommandParts.remove();
 			if(curValue.equals("make")){
-				makeParser(command);
+				while (!mySyntaxCheck.syntaxCheck(curValue)){
+					curValue = curValue + " " + myCommandParts.remove();
+				}
+				System.out.println(curValue);
+				makeParser(curValue);
 			}
 			Node temp = null;
-			if(myCSVTable.returnCSVRow(curValue) == null){
-				temp = new ValueNode(Integer.parseInt(curValue));
-			}
-			else{
-				Class<?> headClass = Class.forName(myCSVTable.returnCSVRow(curValue).getCommandFilePath());
-				temp = (Node) headClass.getConstructors()[0].newInstance();
-				if(temp instanceof TurtleCommandNode){
-					((TurtleCommandNode)temp).setName(curValue);
+				if(myCSVTable.returnCSVRow(curValue) == null){
+					temp = new ValueNode(Integer.parseInt(curValue));
 				}
-				temp.setNumArgs(myCSVTable.returnCSVRow(curValue).getCommandNumArgs());
-			}
-			myCurNodes.add(temp);
+				else{
+					Class<?> headClass = Class.forName(myCSVTable.returnCSVRow(curValue).getCommandFilePath());
+					temp = (Node) headClass.getConstructors()[0].newInstance();
+					if(temp instanceof TurtleCommandNode){
+						((TurtleCommandNode)temp).setName(curValue);
+					}
+					temp.setNumArgs(myCSVTable.returnCSVRow(curValue).getCommandNumArgs());
+				}
+				myCurNodes.add(temp);
 			
-			//if(temp instanceof MakeNode) { myVariables.add(temp); }
 		}
 		Node curNode = myCurNodes.remove();
 		curNode.makeTree(myCurNodes);
@@ -60,9 +68,16 @@ public class Parser {
 	}
 	public void makeParser(String command){
 		try {
-			String varName = command.substring(0,command.indexOf(" "));
-			EncodeTree variableTree = encode(command.substring(command.indexOf(" ")+1));
+			String[] commandParts = command.split(" ");
+			String varName = commandParts[1];
+			String varValue = "";
+			for(int i = 2; i < commandParts.length; i++){
+				varValue += " " + commandParts[i];
+			}
+			System.out.println(varValue);
+			EncodeTree variableTree = encode(varValue);
 			variableTree.evaluate();
+			System.out.println("child value" + variableTree.getHead().getValue());
 			VariableNode aVariable = new VariableNode(variableTree.getHead().getValue(), varName);
 			myVariables.add(aVariable);
 		} catch (Exception e) {
@@ -80,12 +95,7 @@ public class Parser {
 				System.out.println(head.getValue());
 			}
 		
-	}
-	
-	public ArrayList<Node> getVariables(){
-		return myVariables;
-	}
-	
+	}	
 	private String readUserInput(String printMessage) throws IOException{
         System.out.print(printMessage);
         InputStreamReader isr = new InputStreamReader ( System.in );
