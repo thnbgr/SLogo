d88948053a5e8d148dcb91f1f6e8043d2d66b682
Natalia Carvalho <carvalhonatalia@gmail.com>
2013-03-26 00:07:08 -0400

    updated Line.java

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -20,7 +20,6 @@ import java.util.Observer;
 import java.util.ResourceBundle;
 import javax.imageio.ImageIO;
 import javax.swing.AbstractAction;
-import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
--- src/util/Line.java
+++ src/util/Line.java
@@ -20,18 +20,20 @@ public class Line extends JComponent {
     
     /**
      * Constructor for line
-     * @param end 
-     * @param start 
-     * @param startLocation for line
-     * @param endLocation for line
-     * @param color of line
+     * @param start start location for line
+     * @param end end location for line
+     * @param lineColor of line
      */
-    
     public Line (Location start, Location end, Color lineColor) {
         setLocations(start, end);
         myColor = lineColor;
     }
     
+    /**
+     * Set line locations
+     * @param startLocation start location for line
+     * @param endLocation end location for line
+     */
     public void setLocations (Location startLocation, Location endLocation) {
         myStartLocation = startLocation;
         myEndLocation = endLocation;
